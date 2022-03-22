a7cf48c7ca300e9359347489cccdc280a50ea3bf
dough10 <doughten.jimi@gmail.com>
2022-03-21 20:55:45 -0500

    Spelling

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 led_pins = [13,6,19,21,26] # 5 gpio pins using BCM numbering method
-button_pins = [20,16,12,25,24,23] # 6 gpio pins using BCM goip numbering
+button_pins = [20,16,12,25,24,23] # 6 gpio pins using BCM gpio numbering
 keys = ['up','down','left','right','enter',''] # 5 key names and 1 blank string
